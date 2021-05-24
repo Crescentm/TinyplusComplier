f2e3deb7ec9a0c83433746290d2a6952f583d1c1
Crescentm <guyues@outlook.com>
2021-05-25 00:16:52 +0800

    上传symtab.h symtab.cpp

---
+++ symtab.h
@@ -0,0 +1,13 @@
+#ifndef _SYMTAB_H_
+#define _SYMTAB_H_
+
+// 第一次遇到变量时，将变量的lineno和loc插入符号表
+void st_insert(char *name, int lineno, int loc);
+
+// 返回变量的内存位置
+int st_lookup(char *name);
+
+// 将符号表打印到文件
+void printSymTab(FILE *listing);
+
+#endif
