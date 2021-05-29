4329579a61d8ec0b449e3fe6a9e9af037ec361b8
Crescentm <guyues@outlook.com>
2021-05-29 10:23:13 +0800

    主要增加了util.cpp,对global.h进行了修改

--- util.h
+++ util.h
@@ -4,11 +4,14 @@
 // 打印token和token的词素
 void printToken(TokenType, const char *);
 
-// 创建语法树新语句节点
+// 创建语法树语句(Stmt)节点
 TreeNode *newStmtNode(StmtKind);
 
-// 创建语法树的新表达式节点
+// 创建语法树的表达式(exp)节点
 TreeNode *newExpNode(ExpKind);
+
+// 创建语法数的声明变量节点
+TreeNode *newDeclarNode(DeclarKind);
 
 // 创建字符串的新副本
 char *copyString(char *);
